add2ac88953caa204466989ec6f5f581b9331c67
Dhruv Sonavane <sonavanebharat92@gmail.com>
2026-01-14 14:09:30 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include "./include/BPE.hpp"
+#include "./include/BPE.hpp" //Encoding And Decoding with
 using namespace std;
 
 int main()
 {
-    Encoding en;
+    Encoding en; //Class Object
     
     vector<long long> tk;
     vector<string> pair;
     
-    en.fit("../data/test.txt", 100);
-    en.encoding("../data/test2.txt", pair, tk);
+    en.fit("../data/test.txt", 100); //Training
+    en.encoding("../data/test2.txt", pair, tk); // Testing with much of tokens
 
     for (int i = 0; i < pair.size(); ++i) cout << tk[i] << " ";
     cout << endl << endl;
     
-    cout << en.decoding(355) << endl;
+    cout << en.decoding(355) << endl; //Decoding
     
     return 0;
 }
